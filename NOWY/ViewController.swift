53460297c22b2eaf00fd9d250836db95b265a362
Rafał Strzelczyk <Ralf@macbook-pro-rafa.local>
2016-10-04 17:30:09 +0200

    Initial Commit

---
+++ NOWY/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NOWY
+//
+//  Created by Rafał Strzelczyk on 04.10.2016.
+//  Copyright © 2016 Rafał Strzelczyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ NOWYTests/NOWYTests.swift
@@ -0,0 +1,36 @@
+//
+//  NOWYTests.swift
+//  NOWYTests
+//
+//  Created by Rafał Strzelczyk on 04.10.2016.
+//  Copyright © 2016 Rafał Strzelczyk. All rights reserved.
+//
+
+import XCTest
+@testable import NOWY
+
+class NOWYTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
